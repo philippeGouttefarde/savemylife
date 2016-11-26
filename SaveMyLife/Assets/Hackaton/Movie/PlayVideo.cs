0331cf2982c2dd45de9e61a083256a7a701479b5
Philippe GOUTTEFARDE <Philippe GOUTTEFARDE>
2016-11-26 09:07:12 +0100

    Add MP4 management

---
+++ SaveMyLife/Assets/Hackaton/Movie/PlayVideo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayVideo : MonoBehaviour
+{
+    public MovieTexture movTexture;
+    void Start()
+    {
+        GetComponent<RawImage>().material.mainTexture = movTexture;
+        movTexture.loop = true;
+        movTexture.Play();        
+    }
+}
