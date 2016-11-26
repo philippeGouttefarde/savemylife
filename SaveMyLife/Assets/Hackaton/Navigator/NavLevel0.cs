f4118fea7d81e7729fd5a6b2b519573b19e3a079
Philippe GOUTTEFARDE <Philippe GOUTTEFARDE>
2016-11-26 05:07:47 +0100

    Add some Material icons + text animation

--- SaveMyLife/Assets/Hackaton/Navigator/NavLevel0.cs
+++ SaveMyLife/Assets/Hackaton/Navigator/NavLevel0.cs
@@ -4,7 +4,24 @@ using System.Collections;
 public class NavLevel0 : MonoBehaviour
 {
 
-	public void GoToLevel1 ()
+    public float waitingTimeInSecond = 3;
+
+    private void Start()
+    {
+        StartCoroutine("SimulateCall");
+    }
+
+    // every 2 seconds perform the print()
+    private IEnumerator SimulateCall()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitingTimeInSecond);
+            GoToLevel1();
+        }
+    }
+
+    public void GoToLevel1 ()
 	{
 		Application.LoadLevel ("1_Call");
 	}
---
+++ SaveMyLife/Assets/Hackaton/UI/UIAnimatorLevel1.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIAnimatorLevel1 : MonoBehaviour
+{
+    [Header("Animated text")]
+    public Text inCallText;
+    public string T1 = "Appel en cours.";
+    public string T2 = "Appel en cours..";
+    public string T3 = "Appel en cours...";
+    public string T4 = "Appel en cours....";
+
+    private void Start()
+    {
+        StartCoroutine("AnimateText");
+    }
+
+    // every 2 seconds perform the print()
+    private IEnumerator AnimateText()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            float currentTime = Time.time;
+            currentTime -= (int)currentTime;
+
+            Debug.Log("currentTime : " + currentTime);
+
+            if (currentTime <= 0.25f)
+            {
+                inCallText.text = T1;
+            } else if (currentTime <= 0.50f)
+            {
+                inCallText.text = T2;
+            } else if (currentTime <= 0.75f)
+            {
+                inCallText.text = T3;
+            }
+            else
+            {
+                inCallText.text = T4;
+            }
+            Debug.Log("inCallText.text : " + inCallText.text);
+        }
+    }
+
+    public void GoToLevel1 ()
+	{
+		Application.LoadLevel ("1_Call");
+	}
+}
